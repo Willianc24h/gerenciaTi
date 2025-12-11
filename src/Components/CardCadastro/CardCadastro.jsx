8a404fd9f54ae6e2fc8a17d08c4335be5453805a
Willian Carvalho <wsilva@central24horas.com.br>
2025-12-11 10:30:29 -0300

    alterei as rotas para pegar a api em producao

--- src/Components/CardCadastro/CardCadastro.jsx
+++ src/Components/CardCadastro/CardCadastro.jsx
@@ -129,7 +129,7 @@ export default function BasicCard() {
 
     const updatedItem = { ...selectedItem };
 
-    const url = `http://localhost:5108/api/cadastro/${updatedItem.tag}`;
+    const url = `http://192.168.5.32:5108:5108/api/cadastro/${updatedItem.tag}`;
     try {
       const response = await fetch(url, {
         method: "PUT",
