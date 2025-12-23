673467ee1ffb44c96c09cbabbada6980867e6c9f
Willian Carvalho <wsilva@central24horas.com.br>
2025-12-23 09:36:06 -0300

    atualizei as rotas de endpoint

--- src/Components/CardCadastro/CardCadastro.jsx
+++ src/Components/CardCadastro/CardCadastro.jsx
@@ -89,7 +89,7 @@ export default function BasicCard() {
 
     try {
       const response = await fetch(
-        `http://localhost:5108/api/cadastro/inativa/${selectedItem.tag}`,
+        `http://192.168.5.32:5108/api/cadastro/inativa/${selectedItem.tag}`,
         {
           method: "PUT",
         }
@@ -128,7 +128,7 @@ export default function BasicCard() {
 
     const updatedItem = { ...selectedItem };
 
-    const url = `http://localhost:5108/api/cadastro/${updatedItem.tag}`;
+    const url = `http://192.168.5.32:5108/api/cadastro/${updatedItem.tag}`;
     try {
       const response = await fetch(url, {
         method: "PUT",
